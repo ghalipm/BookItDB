e51d6cf728300cb5a09a5c29f1379de134127dc2
Abdujilil <abdujilil0127@hotmail.com>
2021-03-13 12:45:52 -0500

    User feature test updated

--- src/test/java/com/bookit/pages/SelfPage.java
+++ src/test/java/com/bookit/pages/SelfPage.java
@@ -3,6 +3,9 @@ package com.bookit.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SelfPage extends BasePage{
 
     @FindBy(xpath = "//p[.='name']/preceding-sibling::p")
@@ -22,4 +25,20 @@ public class SelfPage extends BasePage{
 
     @FindBy(xpath = "//p[.='side']/preceding-sibling::p")
     public WebElement side;
+
+    @FindBy(xpath = "//h1[.='me']")
+    public WebElement me;
+
+    @FindBy(xpath = "//p[.='team']")
+    public WebElement teamSubtitle;
+
+    public List<WebElement> getUserInfo() {
+        List<WebElement> elements = new ArrayList<>();
+        elements.add(name);
+        elements.add(team);
+        elements.add(role);
+        elements.add(batch);
+        elements.add(campus);
+        return elements;
+    }
 }
--- src/test/java/com/bookit/step_definitions/UserStepDefs.java
+++ src/test/java/com/bookit/step_definitions/UserStepDefs.java
@@ -2,11 +2,19 @@ package com.bookit.step_definitions;
 
 import com.bookit.pages.MapPage;
 import com.bookit.pages.SelfPage;
+import com.bookit.utilities.BrowserUtils;
 import com.bookit.utilities.DBUtils;
+import com.bookit.utilities.Driver;
+import com.bookit.utilities.WaitUtils;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.*;
@@ -29,12 +37,15 @@ public class UserStepDefs {
         Map<String, Object> rowMap = DBUtils.getRowMap(query);
 
         String expectedName = rowMap.get("firstname") + " " + rowMap.get("lastname");
-        String expectedTeam = (String) rowMap.get("team");
+        String expectedTeam = (String) rowMap.get("name");
         String expectedRole = (String) rowMap.get("role");
-        String expectedBatch = "#" + rowMap.get("batch");
-        String expectedCampus = (String) rowMap.get("campus");
+        String expectedBatch = "#" + rowMap.get("batch_number");
+        String expectedCampus = (String) rowMap.get("location");
 
         SelfPage selfPage = new SelfPage();
+
+        WaitUtils.waitUntilVisibilityOfAllElements(selfPage.getUserInfo());
+
         String actualName = selfPage.name.getText().trim();
         String actualTeam = selfPage.team.getText().trim();
         String actualRole = selfPage.role.getText().trim();
